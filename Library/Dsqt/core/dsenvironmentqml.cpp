6ebc984265e9c4ec9a8bd40c3f44d6d459b97784
paulhoux <paulhoux@users.noreply.github.com>
2025-07-25 11:19:18 +0200

    Added platformName to DSEnvironmentQML.

--- Library/Dsqt/core/dsenvironmentqml.cpp
+++ Library/Dsqt/core/dsenvironmentqml.cpp
@@ -1,17 +1,43 @@
 #include "dsenvironmentqml.h"
-namespace dsqt {
-DSEnvironmentQML::DSEnvironmentQML(QObject *parent)
-    : QObject{parent}
-{}
 
-const QString dsqt::DSEnvironmentQML::expand(const QString& string)
-{
+#include "dsqmlapplicationengine.h"
+
+namespace dsqt {
+
+DSEnvironmentQML::DSEnvironmentQML(QObject* parent)
+    : QObject{parent} {
+    auto engine = DSQmlApplicationEngine::DefEngine();
+
+    connect(
+        engine, &DSQmlApplicationEngine::rootUpdated, this,
+        [this, engine]() {
+            // Platform name.
+            auto content   = engine->getContentRoot();
+            auto platforms = content.getChildByName("platforms");
+            auto platform  = platforms.getChild(0);
+            setPlatformName(platform.getPropertyString("record_name"));
+        },
+        Qt::ConnectionType::QueuedConnection);
+}
+
+const QString dsqt::DSEnvironmentQML::expand(const QString& string) {
     const std::string val = DSEnvironment::expand(string.toStdString());
     return QString::fromStdString(val);
 }
 
-const QUrl DSEnvironmentQML::expandUrl(const QString &string)
-{
+const QUrl DSEnvironmentQML::expandUrl(const QString& string) {
     return QUrl(expand(string));
 }
+
+QString DSEnvironmentQML::getPlatformName() const {
+    return m_platformName;
 }
+
+void DSEnvironmentQML::setPlatformName(const QString& name) {
+    if (!name.isEmpty() && name != m_platformName) {
+        m_platformName = name;
+        emit platformNameChanged();
+    }
+}
+
+} // namespace dsqt
--- Library/Dsqt/core/dsenvironmentqml.h
+++ Library/Dsqt/core/dsenvironmentqml.h
@@ -4,20 +4,32 @@
 #include <QObject>
 #include <QQmlEngine>
 #include <dsenvironment.h>
+
 namespace dsqt {
-class DSEnvironmentQML : public QObject
-{
+
+class DSEnvironmentQML : public QObject {
     Q_OBJECT
     QML_ELEMENT
     QML_UNCREATABLE("Aint no need for you to be making one.")
-public:
-    explicit DSEnvironmentQML(QObject *parent = nullptr);
-    Q_INVOKABLE const QString expand(const QString& string);
-    Q_INVOKABLE const QUrl expandUrl(const QString& string);
-    //Q_INVOKABLE QString getLocalFolder();
-signals:
 
+    Q_PROPERTY(QString platformName READ getPlatformName WRITE setPlatformName NOTIFY platformNameChanged)
+
+  public:
+    explicit DSEnvironmentQML(QObject* parent = nullptr);
+    Q_INVOKABLE const QString expand(const QString& string);
+    Q_INVOKABLE const QUrl    expandUrl(const QString& string);
+    // Q_INVOKABLE QString getLocalFolder();
+
+    QString getPlatformName() const;
+    void    setPlatformName(const QString& name);
+
+  signals:
+    void platformNameChanged(); // Signal emitted when platformName changes.
+
+  private:
+    QString m_platformName{"Unknown Platform"};
 };
-}//namespace dsqt
+
+} // namespace dsqt
 
 #endif // DSENVIRONMENTQML_H
