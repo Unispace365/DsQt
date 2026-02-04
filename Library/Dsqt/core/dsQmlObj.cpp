e9f7b1e61dcd871920457faf23b26b583816aff8
afrancois <aubrey.francois@downstream.com>
2026-02-04 13:07:53 -0800

    [B] fix recordById

--- Library/Dsqt/core/dsQmlObj.cpp
+++ Library/Dsqt/core/dsQmlObj.cpp
@@ -48,7 +48,12 @@ DsQmlPathHelper* DsQmlObj::path() const {
 
 model::ContentModel* DsQmlObj::getRecordById(const QString& id) const {
     auto& lookup = model::ContentLookup::get();
-    return lookup.find(id).value();
+    auto itr = lookup.find(id);
+    if(itr!=lookup.end()){
+        return lookup.find(id).value();
+    } else {
+        return nullptr;
+    }
 }
 
 void DsQmlObj::updatePlatform() {
