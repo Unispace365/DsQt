be4b4cba200b40772d17916c079cf3c7be8ff39a
afrancois <aubrey.francois@downstream.com>
2026-02-18 00:55:49 -0800

    Fix misconfiguration of Bridge service signal connections.
    the DsBridge object should now fire a bridgeUpdated signal when the bridge content changes.

--- Library/DsQt/Bridge/bridge/dsQmlBridge.h
+++ Library/DsQt/Bridge/bridge/dsQmlBridge.h
@@ -3,17 +3,20 @@
 
 #include "bridge/dsBridgeDatabase.h"
 #include "model/dsContentModel.h"
+#include "settings/dsSettings.h"
 
 #include <QObject>
 #include <QQmlEngine>
 
+
 namespace dsqt::bridge {
+//TODO: logging category for this class
 
 class DsQmlBridge : public QObject {
     Q_OBJECT
     QML_SINGLETON
     QML_NAMED_ELEMENT(DsBridge)
-    Q_PROPERTY(model::ContentModel* content READ content WRITE setContent NOTIFY contentChanged FINAL)
+    Q_PROPERTY(dsqt::model::ContentModel* content READ content NOTIFY contentChanged FINAL)
 
   public:
     static DsQmlBridge& instance() {
@@ -26,6 +29,10 @@ class DsQmlBridge : public QObject {
     dsqt::model::ContentModel* content() const;
 
     void setContent(dsqt::model::ContentModel* newContent);
+
+    static DsQmlBridge* create(QQmlEngine *, QJSEngine *){
+        return &instance();
+    }
 
     /**
      * @brief Returns the Bridge database content in a thread-safe manner.
@@ -40,7 +47,71 @@ class DsQmlBridge : public QObject {
     void setDatabase(bridge::DatabaseContent&& database) {
         m_database = std::move(database);
         emit databaseChanged();
+        emit bridgeUpdated();
     }
+
+    /**
+     * @brief Get ContentModel by Id.
+     * @param id The unique identifier of the content record.
+     * @return Pointer to ContentModel if found, nullptr otherwise.
+     */
+    Q_INVOKABLE model::ContentModel* getRecordById(const QString& id) const {
+        auto record = m_database.find(id);
+        if (record.isEmpty()) return nullptr;
+        return model::ContentModel::create(record);
+    }
+
+    /**
+     * @brief Get uid of platform from app_settings platform.id
+     * @return QString uid of platform if found, empty QString otherwise.
+     */
+    Q_INVOKABLE QString getPlatformUid() const {
+        auto platformId = DsSettings::getSettings("app_settings")->getOr<QString>("platform.id", "");
+        if (platformId.isEmpty()) {
+            qDebug()<<"Attempting to get platform uid but platform.id is not set in app_settings";
+            return "";
+        }
+        for (const auto& platform : m_database.platforms()) {
+            if (platform.value("id").toString() == platformId) {
+                return platform.value("uid").toString();
+            }
+        }
+        qDebug()<<"Attempting to get platform uid but no platform with id "<<platformId<<" found in database";
+        return "";
+    }
+
+    /**
+     * @brief Get uids of platforms
+     * @return QStringList uid of platforms.
+     */
+    Q_INVOKABLE QStringList getPlatformUids() const {
+        QStringList uids;
+        for (const auto& platform : m_database.platforms()) {
+            uids.append(platform.value("uid").toString());
+        }
+        return uids;
+    }
+
+    /**
+     * @brief Get ContentModel of platform by id from app_settings platform.id
+     * @return Pointer to ContentModel if found, nullptr otherwise.
+     */
+    Q_INVOKABLE model::ContentModel* getPlatformRecord() const {
+        auto platformId = DsSettings::getSettings("app_settings")->getOr<QString>("platform.id", "");
+        if (platformId.isEmpty()) {
+            qDebug()<<"Attempting to get platform record but platform.id is not set in app_settings";
+            return nullptr;
+        }
+        for (const auto& platform : m_database.platforms()) {
+            if (platform.value("id").toString() == platformId) {
+                return model::ContentModel::create(platform);
+            }
+        }
+        qDebug()<<"Attempting to get platform record but no platform with id "<<platformId<<" found in database";
+        return nullptr;
+    }
+
+
 
   signals:
     /**
@@ -52,6 +123,11 @@ class DsQmlBridge : public QObject {
      * @brief Emitted when the root content is updated. Thread-safe version.
      */
     void databaseChanged();
+
+    /**
+     * @brief Emitted when the bridge is updated. This is a general signal that can be used to indicate any update to the bridge, including content and database changes. It is thread-safe.
+     */
+    void bridgeUpdated();
 
   private:
     DsQmlBridge();
