ddf1cdc9f504a801fc7af0ee6362098cbc48ae50
Paul Houx <paul@cowlumbus.nl>
2026-06-11 16:35:08 +0200

    Swapped in the new settings files. Changed how TOML++ is included. Also added Widgets as component.

--- Library/DsQt/Core/settings/dsQmlSettingsProxy.h
+++ Library/DsQt/Core/settings/dsQmlSettingsProxy.h
@@ -1,53 +1,74 @@
-#ifndef DSQMLSETTINGPROXY_H
-#define DSQMLSETTINGPROXY_H
+#pragma once
 
-#include "settings/dsSettings.h"
-
+#include <QColor>
+#include <QDateTime>
 #include <QObject>
-#include <qqmlintegration.h>
+#include <QPointF>
+#include <QQmlEngine>
+#include <QQuaternion>
+#include <QRectF>
+#include <QSizeF>
+#include <QVariant>
+#include <QVariantList>
+#include <QVariantMap>
+#include <QVector3D>
+#include <QVector4D>
 
 namespace dsqt {
-///
-/// \brief The DSSettingsProxy class
-///
-class DsQmlSettingsProxy : public QObject
-{
-	Q_OBJECT
-	Q_PROPERTY(QString target READ target WRITE setTarget NOTIFY targetChanged);
-	Q_PROPERTY(QString prefix READ prefix WRITE setPrefix NOTIFY prefixChanged);
+
+class DsQmlSettingsProxy : public QObject {
+    Q_OBJECT
     QML_NAMED_ELEMENT(DsSettingsProxy)
-  public:
-    explicit DsQmlSettingsProxy(QObject *parent = nullptr);
 
-	QString target(){return _target;}
-	void setTarget(const QString& val);
+    Q_PROPERTY(QString target READ target WRITE setTarget NOTIFY targetChanged)
+    Q_PROPERTY(QString prefix READ prefix WRITE setPrefix NOTIFY prefixChanged)
 
-	QString prefix(){return _prefix;}
-	void setPrefix(const QString& val);
+public:
+    explicit DsQmlSettingsProxy(QObject* parent = nullptr);
 
-	Q_INVOKABLE void loadFromFile(const QString& filename);
-	Q_INVOKABLE QVariant getString(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getInt(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getFloat(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getDate(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getPoint(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getRect(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getSize(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getColor(const QString& key,QVariant def = QVariant{});
-	Q_INVOKABLE QVariant getBool(const QString& key, QVariant def = QVariant{});
+    QString target() const { return m_target; }
+    // Sets the name of the settings file to use. Calls Settings::add(target)
+    // to ensure the file is registered and loaded before the first lookup.
+    void setTarget(const QString& target);
 
-    Q_INVOKABLE QVariantList getList(const QString &key, QVariant def = QVariant{});
-    Q_INVOKABLE QVariantMap getObj(const QString &key, QVariant def = QVariant{});
+    QString prefix() const { return m_prefix; }
+    // Sets the key prefix. Every key passed to get*() is looked up as
+    // "<prefix>.<key>" (or just "<key>" when prefix is empty).
+    void setPrefix(const QString& prefix);
+
+    Q_INVOKABLE QVariant getString  (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getInt     (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getFloat   (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getBool    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getDate    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getTime    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getDateTime(const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getColor   (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getPoint   (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getSize    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getRect    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getVec3    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getVec4    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariant getQuat    (const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariantList getList(const QString& key, const QVariant& def = {}) const;
+    Q_INVOKABLE QVariantMap  getObj (const QString& key, const QVariant& def = {}) const;
+
 signals:
-	void targetChanged(QString target);
-	void prefixChanged(QString prefix);
+    void targetChanged();
+    void prefixChanged();
 
-  private:
-	QString _target;
-	QString _prefix="";
-	std::string _prefix_str="";
-	DsSettingsRef _settings;
+private:
+    // Returns prefix + "." + key, or just key when prefix is empty.
+    QString fullKey(const QString& key) const;
 
+    // Fetches the raw value via Settings::find<QVariant>, then converts to T.
+    // Returns def unchanged if the target is unset, the key is missing, or
+    // the value cannot be converted to T.
+    template<typename T>
+    QVariant getAs(const QString& key, const QVariant& def) const;
+
+    QString m_target;
+    QString m_prefix;
 };
-}
-#endif // DSSETTINGPROXY_H
+
+} // namespace dsqt
